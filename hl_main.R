da90393d982c3b93f168643f75447b0c27d8e282
peterkuriyama <ptrkrym@uw.edu>
2015-09-15 16:47:53 -0700

    update

--- hl_main.R
+++ hl_main.R
@@ -3,6 +3,10 @@
 
 setwd('/Users/peterkuriyama/School/Research/hook_and_line/')
 
-load('data/')
+#Data stored locally
+# load('data/Grand 2012 27 Apr 2013.dmp')
+load('data/Grand.2014.JF.dmp')
+dat <- Grand.2014.JF
+
 
 
